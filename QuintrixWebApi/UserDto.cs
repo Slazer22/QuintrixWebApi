112cd1db2ba3a09948c4146651b110c53bed8b0a
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-06-15 17:40:24 -0500

    Add project files.

---
+++ QuintrixWebApi/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace QuintrixWebApi
+{
+    public class UserDto
+    {
+        //For user registration. 
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
